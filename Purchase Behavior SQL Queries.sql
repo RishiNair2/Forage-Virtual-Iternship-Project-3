fe400ae7db59d652104f7c3314fb3a5909d98565
RishiNair2 <123101414+RishiNair2@users.noreply.github.com>
2023-02-09 19:30:30 -0500

    Update Purchase Behavior SQL Queries.sql

--- Purchase Behavior SQL Queries.sql
+++ Purchase Behavior SQL Queries.sql
@@ -15,22 +15,18 @@ GROUP BY lifestage
 ORDER BY num_customers DESC;
 
 --- Proportion Of Each Customer Type to the overall customer base---
-SELECT customer_type, COUNT(loyalty_card_number) *100/(SELECT SUM(t1.total_customers)
-													FROM (
-													SELECT customer_type, COUNT(loyalty_card_number) total_customers
-													FROM purchase_behavior
-													GROUP BY customer_type
-													ORDER BY COUNT(loyalty_card_number) DESC
-													LIMIT 5) t1) percent_of_total
+SELECT customer_type, COUNT(loyalty_card_number) *100/(SELECT SUM(t1.total_customers) FROM (SELECT customer_type, COUNT(loyalty_card_number) total_customers
+														FROM purchase_behavior
+														GROUP BY customer_type
+														ORDER BY COUNT(loyalty_card_number) DESC
+														LIMIT 5) t1) percent_of_total
 FROM purchase_behavior
 GROUP BY customer_type
 ORDER BY percent_of_total DESC
 LIMIT 5;
 
 ---Proportion Of Each Lifestage to the overall customer base---
-SELECT lifestage, COUNT(loyalty_card_number) *100/(SELECT SUM(t1.total_customers)
-													FROM (
-													SELECT lifestage, COUNT(loyalty_card_number) total_customers
+SELECT lifestage, COUNT(loyalty_card_number) *100/(SELECT SUM(t1.total_customers) FROM (SELECT lifestage, COUNT(loyalty_card_number) total_customers
 													FROM purchase_behavior
 													GROUP BY lifestage
 													ORDER BY COUNT(loyalty_card_number) DESC
@@ -40,9 +36,7 @@ GROUP BY lifestage
 ORDER BY percent_of_total DESC;
 
 ---- Proportion of Customers by Lifestage and Premium Customer----
-SELECT lifestage, customer_type, COUNT(loyalty_card_number) *100/(SELECT SUM(t1.total_customers)
-													FROM (
-													SELECT lifestage, customer_type, COUNT(loyalty_card_number) total_customers
+SELECT lifestage, customer_type, COUNT(loyalty_card_number) *100/(SELECT SUM(t1.total_customers) FROM ( SELECT lifestage, customer_type, COUNT(loyalty_card_number) total_customers
 													FROM purchase_behavior
 													GROUP BY lifestage, customer_type
 													ORDER BY COUNT(loyalty_card_number) DESC
